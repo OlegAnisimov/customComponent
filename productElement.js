02303d87b4a7de4f396c01166345f09f568d235f
Oleg Anisimov <o.anisimov@spacevm.org>
2025-07-15 18:40:24 +0300

    product element

--- productElement.js
+++ productElement.js
@@ -42,38 +42,67 @@ class productElement extends HTMLElement {
     windowResizeListener: null,
   }
 
+  productContainerStyleSheet = new CSSStyleSheet();
+
+  initialDesignType;
+  
   connectedCallback() {
     console.log("Custom element added to page.");
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = `<div class="product"></div>`
     console.log(shadow);
+
+    const productContainer = document.createElement('div');
+    productContainer.classList.add('product-container');
+    shadow.appendChild(productContainer);
     
     const setDefaultDesign = () => {
       if (window.innerWidth <= this.breakpoints.mobile) {
-        shadow.insertAdjacentHTML('beforeend', this.mobileDefaultView);
+        productContainer.innerHTML = this.mobileDefaultView;
       } else {
-        shadow.insertAdjacentHTML('beforeend', this.desctopDefaultView);
+        productContainer.innerHTML = this.desctopDefaultView; 
       }
     }
 
+// 
+    this.productContainerStyleSheet.replaceSync(`.product-container {
+  display: block;
+  height: 400px;
+  width: 400px;
+  border: 1px red solid;
+}`);
+
+  shadow.adoptedStyleSheets = [this.productContainerStyleSheet];
+    
     this.eventsHandlers.windowResizeListener = window.addEventListener('resize', setDefaultDesign);
-
+    
     setDefaultDesign();
-
+    // if (this.getAttribute('data-product-name')) {
+    //   shadow.innerHTML = this.getAttribute('data-product-name');
+    // }
+    // console.log(this.parentElement);
+    
 
     this.addEventListener('mouseenter', (e) => {
-      // e.preventDefault();
-      // if (this.hasAttribute('data-versions-list')) {
-      //   shadow.innerHTML = `
-      //     <div>
-      //       ${this.getAttribute('data-versions-list')}
-      //     </div>
-      //   `;
-      // }
-      // console.log('hover', e);
+      if (this.hasAttribute('data-versions-list')) {
+        e.stopImmediatePropagation(); //TODO: research it
+        // всегла на ивент будет создавать versionsList - есть смысл создать до тригерра ивента
+        const versionsList = document.createElement('div');
+        console.log(typeof this.getAttribute('data-versions-list'));
+        
+        this.getAttribute('data-versions-list').split(',').forEach(version => {
+          versionsList.insertAdjacentHTML('afterbegin', `
+              <a class="version"> Версия ${version}</a>
+            `)
+        });
+        
+        productContainer.innerHTML = versionsList.innerHTML;        
+      }
     });
 
+    this.addEventListener('mouseleave', () => {
+      setDefaultDesign();
+    })
   }
 
   disconnectedCallback() {
